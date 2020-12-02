ca0afb82fc641ba9896458c0603463660ccd93ce
Jerald Mogo <jmogo@va-b5.kzn.21-school.ru>
2020-12-02 16:22:23 +0300

    pushing to my git

---
+++ get_next_line.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmogo <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/08 16:07:18 by jmogo             #+#    #+#             */
+/*   Updated: 2020/11/21 18:20:33 by jmogo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <unistd.h>
+
+int		get_next_line(int fd, char **line);
+size_t	ft_strlen(char *s);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strchr(char	*s, char c);
+char	*ft_strdup(char *s);
+int		free_str(char *s, char *arr);
+int		ft_do_arr(char **arr, char **line, size_t got);
+int		check(int fd, char **line, char **s);
+int		ft_spec(char **arr, char *npos, char **line);
+
+#endif
---
+++ get_next_line_bonus.c
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmogo <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/21 18:19:00 by jmogo             #+#    #+#             */
+/*   Updated: 2020/11/21 18:20:19 by jmogo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+int	ft_spec(char **arr, char *npos, char **line)
+{
+	char	*tmp;
+
+	tmp = *arr;
+	*npos = '\0';
+	*line = ft_strdup(*arr);
+	*arr = ft_strdup((npos + 1));
+	free(tmp);
+	return (1);
+}
+
+int	ft_do_arr(char **arr, char **line, size_t got)
+{
+	char	*npos;
+
+	if (!got && !(*arr))
+	{
+		*line = ft_strdup("");
+		return (0);
+	}
+	if (got < 0)
+		return (free_str(*arr, 0x0));
+	if ((npos = ft_strchr(*arr, '\n')))
+		return (ft_spec(arr, npos, line));
+	else if ((npos = ft_strchr(*arr, '\0')))
+	{
+		*line = ft_strdup(*arr);
+		free(*arr);
+		*arr = 0x0;
+	}
+	return (0);
+}
+
+int	check(int fd, char **line, char **s)
+{
+	int	got;
+
+	if (fd < 0 || fd > 1025 || BUFFER_SIZE < 1 || !line)
+		return (1);
+	if (0x0 == (*s = malloc(sizeof(char) * (BUFFER_SIZE + 1))))
+		return (1);
+	got = read(fd, *s, 0);
+	if (got < 0)
+		return (free_str(*s, 0x0));
+	return (0);
+}
+
+int	get_next_line(int fd, char **line)
+{
+	static char	*arr[1025];
+	char		*str;
+	int			got;
+
+	str = 0x0;
+	if (check(fd, line, &str))
+		return (-1);
+	while (0 < (got = read(fd, str, BUFFER_SIZE)))
+	{
+		str[got] = '\0';
+		if (!(arr[fd] = ft_strjoin(arr[fd], str)))
+			return (free_str(arr[fd], str));
+		if ((ft_strchr(arr[fd], '\n')))
+			break ;
+	}
+	free(str);
+	str = 0x0;
+	return (ft_do_arr(&arr[fd], line, got));
+}
